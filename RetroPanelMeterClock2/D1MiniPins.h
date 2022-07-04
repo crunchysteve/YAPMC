f9dd214dc3adf8c281ee867b4eb5021a2f0f8499
crunchysteve <46626696+crunchysteve@users.noreply.github.com>
2022-07-05 09:48:39 +1000

    Update D1MiniPins.h
    Move pin constants from main file to this file.

--- RetroPanelMeterClock2/D1MiniPins.h
+++ RetroPanelMeterClock2/D1MiniPins.h
@@ -9,4 +9,13 @@
     #define D6 12
     #define D7 13
     #define D8 15
+
+/***   D1mini Specific Contants   ***/
+    const int A_PIN = D0;   //  AM/PM light pin
+    const int M_PWM = D1;   //  Minutes meter pin
+    const int H_PWM = D2;   //  Hours meter pin
+    const int M_SET = D5;   //  Minutes set button pin
+    const int H_SET = D6;   //  Hours set button pin
+    const int S_PIN = D7;   //  Seconds light pin
+
 #endif
