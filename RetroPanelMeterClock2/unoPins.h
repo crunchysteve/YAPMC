bf8eff2df2c2915d24e04d8ed57a76e3ff60e25d
crunchysteve <46626696+crunchysteve@users.noreply.github.com>
2022-07-05 09:36:22 +1000

    Create unoPins.h
    New Pins def for UNO boards

---
+++ RetroPanelMeterClock2/unoPins.h
@@ -0,0 +1,23 @@
+#ifndef D1MiniPins_h
+  #define D1MiniPins_h
+    #define  D0  0
+    #define  D1  1
+    #define  D2  2
+    #define  D3  3
+    #define  D4  4
+    #define  D5  5
+    #define  D6  6
+    #define  D7  7
+    #define  D8  8
+    #define  D9  9
+    #define D10 10
+    #define D11 11
+    #define D12 12
+    #define D13 13
+    #define  A0 14
+    #define  A1 15
+    #define  A2 16
+    #define  A3 17
+    #define  A4 18
+    #define  A5 19
+#endif
