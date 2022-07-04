e8d952089bebaa0384616e3989b88a6ad7fa7741
crunchysteve <46626696+crunchysteve@users.noreply.github.com>
2022-07-05 09:37:29 +1000

    Update D1MiniPins.h
    tidying formatting

--- RetroPanelMeterClock2/D1MiniPins.h
+++ RetroPanelMeterClock2/D1MiniPins.h
@@ -1,10 +1,10 @@
 #ifndef D1MiniPins_h
   #define D1MiniPins_h
     #define D0 16
-    #define D1 5
-    #define D2 4
-    #define D3 0
-    #define D4 2
+    #define D1  5
+    #define D2  4
+    #define D3  0
+    #define D4  2
     #define D5 14
     #define D6 12
     #define D7 13
